798f9ebdd154c273b6ec1d37fa90789ef1f489c2
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-07 12:38:35 +0200

    Added discount and bonus

--- bootstrap-portfolio-starter/bootstrap-portfolio/js/main.js
+++ bootstrap-portfolio-starter/bootstrap-portfolio/js/main.js
@@ -1,3 +1,6 @@
+let discountCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
 function calculatorWorkPrice(event){
     event.preventDefault();
 
@@ -30,7 +33,35 @@ function calculatorWorkPrice(event){
         case "projectanalysis":
             price = projectAnalysisPrice * selectedHours;
             break;
-    }   
+    } 
+    
+    
+    let userDiscountCode = document.getElementById("discountcode").value;
+
+    let discountCodeExist = discountCode.includes(userDiscountCode);
+
+    if(discountCodeExist){
+        price = price * 0.75;
+        discountCode = removeElementFromArray(discountCode, userDiscountCode);
+
+        console.log(discountCode);
+
+        document.getElementById("applieddiscount").innerHTML = "Il codice sconto del 25% è stato applicato!";
+    } else if (userDiscountCode != "") {
+        document.getElementById("discountcode").classList.add("text-danger");
+        document.getElementById("applieddiscount").innerHTML = "Il codice sconto non è valido!";
+    } else {
+        document.getElementById("applieddiscount").innerHTML = "";
+    }
 
     document.getElementById("price").innerHTML = price.toFixed(2);
+}
+
+function removeElementFromArray(array, elementToRemove){
+
+    const index = array.indexOf(elementToRemove);
+
+    array.splice(index, 1);
+
+    return array;
 }
